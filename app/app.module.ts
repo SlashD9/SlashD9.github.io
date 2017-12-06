1de1689143002a5ab28eaa5fe498ec00dddde4be
SlashD9 <colin.p.s.daly@gmail.com>
2017-12-06 23:31:46 +0000

    First Commit

---
+++ app/app.module.ts
@@ -0,0 +1,91 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, Validators } from '@angular/forms';
+import { DataService } from './Services/data.services';
+
+/* ngx-bootstrap */
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { CarouselComponent } from 'ngx-bootstrap/carousel/carousel.component';
+import { AlertModule } from 'ngx-bootstrap';
+import { BsModalService } from 'ngx-bootstrap/modal/bs-modal.service';
+import { ComponentLoaderFactory } from 'ngx-bootstrap/component-loader/component-loader.factory';
+import { PositioningService } from 'ngx-bootstrap/positioning/positioning.service';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
+/* Videogular */
+import { VgCoreModule } from 'videogular2/core';
+import { VgControlsModule } from 'videogular2/controls';
+import {VgOverlayPlayModule} from 'videogular2/overlay-play';
+import {VgBufferingModule} from 'videogular2/buffering';
+
+/* Components */
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { BandComponent } from './components/band/band.component';
+import { MusicComponent } from './components/music/music.component';
+import { VideoComponent } from './components/video/video.component';
+import { PhotosComponent } from './components/photos/photos.component';
+import { DavyComponent } from './components/davy/davy.component';
+import { MickyComponent } from './components/micky/micky.component';
+import { MichaelComponent } from './components/michael/michael.component';
+import { PeterComponent } from './components/peter/peter.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ToursComponent } from './components/tours/tours.component';
+import { BookingComponent } from './components/booking/booking.component';
+
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full'},
+  { path: 'band', component: BandComponent, pathMatch: 'full'},
+  { path: 'video', component: VideoComponent, pathMatch: 'full'},
+  { path: 'music', component: MusicComponent, pathMatch: 'full'},
+  { path: 'photos', component: PhotosComponent, pathMatch: 'full'},
+  { path: 'davy', component: DavyComponent, pathMatch: 'full'},
+  { path: 'micky', component: MickyComponent, pathMatch: 'full'},
+  { path: 'michael', component: MichaelComponent, pathMatch: 'full'},
+  { path: 'peter', component: PeterComponent, pathMatch: 'full'},
+  { path: 'contact', component: ContactComponent, pathMatch: 'full'},
+  { path: 'tours', component: ToursComponent, pathMatch: 'full'},
+  { path: 'booking', component: BookingComponent, pathMatch: 'full'}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavBarComponent,
+    BandComponent,
+    MusicComponent,
+    VideoComponent,
+    PhotosComponent,
+    DavyComponent,
+    MickyComponent,
+    MichaelComponent,
+    PeterComponent,
+    ContactComponent,
+    ToursComponent,
+    BookingComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule,
+    CarouselModule.forRoot(),
+    AlertModule,
+    VgCoreModule,
+    ModalModule,
+    BsDropdownModule.forRoot()
+  ],
+  providers: [
+    DataService,
+    BsModalService,
+    ComponentLoaderFactory,
+    PositioningService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
